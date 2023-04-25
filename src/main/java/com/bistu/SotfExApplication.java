2d8ec3280a7498b1051590d9fb36094452de2d06
GHremedy <remedy@qwe.com>
2023-04-25 22:46:39 +0800

    项目创建

---
+++ src/main/java/com/bistu/SotfExApplication.java
@@ -0,0 +1,13 @@
+package com.bistu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SotfExApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SotfExApplication.class, args);
+    }
+
+}
